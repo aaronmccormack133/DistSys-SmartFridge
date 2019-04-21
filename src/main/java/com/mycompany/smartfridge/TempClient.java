efebec1f66fe2e9c77c97e98e12c7ddcb7dd6d03
aaronmccormack15533 <maron.accormack@gmail.com>
2019-04-21 16:37:42 +0100

    client working

---
+++ src/main/java/com/mycompany/smartfridge/TempClient.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.smartfridge;
+
+import io.grpc.*;
+
+/**
+ *
+ * @author Aaron
+ */
+public class TempClient {
+    public static void main(String args[]) throws Exception {
+        final ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext(true)
+                .build();
+        
+        TempServiceGrpc.TempServiceBlockingStub stub = TempServiceGrpc.newBlockingStub(channel);
+        Fridgetemp.TempRequest request = Fridgetemp.TempRequest.newBuilder()
+                .setName("10")
+                .build();
+        
+        Fridgetemp.TempReply response = stub.temp(request);
+        
+        System.out.println(response);
+        
+        channel.shutdown();
+    }
+}
